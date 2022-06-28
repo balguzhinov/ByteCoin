d342f053e7897c761d633d74835e86e4772b2afe
Balguzhinov Abay <75177898+balguzhinov@users.noreply.github.com>
2022-06-28 16:57:55 +0600

    Add files via upload

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Абай on 19/02/2022.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    var rate: Double
+    var asset_id_quote: String
+}
+
